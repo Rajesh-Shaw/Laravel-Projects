c6330eca200d29a13c931e06a104a6cd61a3cc98
Rajesh-Shaw <rkshaw7200@gmail.com>
2026-01-13 23:49:10 +0530

    Update page post

--- 03-Layout-Based-Page-Manager/resources/views/pages/post.blade.php
+++ 03-Layout-Based-Page-Manager/resources/views/pages/post.blade.php
@@ -3,4 +3,5 @@
 @section('title', 'Post Page')
 @section('content')
     <h1>Post ID: {{ $id }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laboriosam corrupti nihil dolores, eveniet cupiditate aut eligendi ad omnis in neque a sit quibusdam at?</p>
 @endsection
